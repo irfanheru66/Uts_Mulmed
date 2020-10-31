816b517a9112dbcdefd9f38dcfcddae0c5c28e40
Irfan Heru Pratomo <60539965+irfanheru66@users.noreply.github.com>
2020-10-31 20:21:38 +0700

    Nambahin Gambar untuk semua materi

--- Unit1.cpp
+++ Unit1.cpp
@@ -23,10 +23,12 @@ FUtama->Hide();
 
 FMateri->LblJdl->Caption = "Kenalan yuk sama Smart Waste Management!"  ;
 FMateri->Teks->Lines->LoadFromFile("1Definisi.txt");
+FMateri->MateriImg->Picture->LoadFromFile("Definisi.bmp");
+
+FMateri->Imname[1] = "Definisi-2.bmp";
 FMateri->Filename[1] = "1Definisi2.txt";
 FMateri->Timer1->Enabled = true;
 FMateri->Timer1->Interval = 5000;
-
 
 
 
@@ -34,7 +36,7 @@ FMateri->Timer1->Interval = 5000;
 //---------------------------------------------------------------------------
 void __fastcall TFUtama::BitBtn5Click(TObject *Sender)
 {
-Close();        
+Close();
 }
 //---------------------------------------------------------------------------
 void __fastcall TFUtama::BitBedaClick(TObject *Sender)
@@ -42,7 +44,8 @@ void __fastcall TFUtama::BitBedaClick(TObject *Sender)
   FMateri->Show();
 FUtama->Hide();
 
-FMateri->LblJdl->Caption = "Kenapa sih Smart Waste Management ini keren??"  ;
+FMateri->LblJdl->Caption = "Kenapa sih Smart Waste Management ini keren??";
+FMateri->MateriImg->Picture->LoadFromFile("Perbedaan.bmp");
 
 FMateri->Teks->Lines->LoadFromFile("3Perbedaan.txt");
 }
